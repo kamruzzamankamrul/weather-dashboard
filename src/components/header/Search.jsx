51e3c6432b1b9072f6135e3f6d3b4f44efd53985
kamruzzaman <kamruzzamankamrul24158@gmail.com>
2025-06-01 09:24:03 +0600

    updated search pattern

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -1,41 +1,31 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import SearchIcon from "../../assets/search.svg";
 import { LocationContext } from "../../context";
 import { useDebounce } from "../../hooks";
 
 export default function Search() {
-  const [searchTerm, setSearchTerm] = useState("");
   const { setSelectedLocation, error, setError } = useContext(LocationContext);
 
-  
   const doSearch = useDebounce((term) => {
     setSelectedLocation(term);
   }, 1000);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSelectedLocation(searchTerm); // Immediate search on submit
-  };
-
   const handleChange = (e) => {
     const value = e.target.value;
-    setSearchTerm(value); // Update input value
-    doSearch(value);      // Trigger debounced search
-    
+    doSearch(value);
+
     if (error) setError("");
   };
 
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div className="flex flex-col w-full">
           <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
             <input
               className="bg-transparent placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
               type="search"
               placeholder="Search Location"
-              // value={searchTerm}
               onChange={handleChange}
               required
             />
